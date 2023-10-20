c23c9f8f45ea17f82b8aff1e277c25eab1ce105d
yakoub abaya <sting606@gmail.com>
2023-10-20 20:40:57 +0300

    better moderation handling, fix labels to indicate sync state

--- translations_moderation_pack/js/sync_states.js
+++ translations_moderation_pack/js/sync_states.js
@@ -1,9 +1,11 @@
 (function(Drupal) {
 Drupal.behaviors.translation_moderation_tabs = {
+  initialized: false,
   attach: function(context, settings) {
-    if (context.nodeName != '#document') {
+    if (context.nodeName != '#document' || this.initialized) {
       return;
     }
+    this.initialized = true;
     var selector = 'form div.translation-pack.field-moderation_state';
     var wrapper = document.querySelector(selector);
     if (!wrapper) {
@@ -13,15 +15,25 @@ Drupal.behaviors.translation_moderation_tabs = {
     var sync_states = wrapper.querySelector('input.sync-states[type="checkbox"]');
     var master_select = wrapper.querySelector('.field--name-moderation-state.active select');
     var other_selects = wrapper.querySelectorAll('.field--name-moderation-state:not(.active) select');
+    var master_label_language = '';
+    var pack_tabs = Drupal.behaviors.translation_tabs.tabs;
 
     if (sync_states.checked) {
       wrapper.querySelectorAll('[data-lang-pack]').forEach(function(elm) {
         var code = elm.dataset.langPack;
         elm.classList.remove('field-language-' + code);
       });
+      
       var active = wrapper.querySelector('.active[data-lang-pack]');
       active.classList.remove('active');
       active.style.display = 'block';
+      var labels = active.querySelectorAll('label');
+      for (let label_iter of labels) {
+        master_label_language = label_iter.textContent.match(/ \(.+\)$/);
+        if (master_label_language) {
+          label_iter.textContent = label_iter.textContent.replace(master_label_language[0], '');
+        }
+      }
     }
 
     sync_states.addEventListener('change', function(event) {
@@ -32,18 +44,41 @@ Drupal.behaviors.translation_moderation_tabs = {
           if (code == 'original') {
             elm.classList.remove('active');
             elm.style.display = 'block';
+
+            var labels = elm.querySelectorAll('label');
+            for (let label_iter of labels) {
+              master_label_language = label_iter.textContent.match(/ \(.+\)$/);
+              if (master_label_language) {
+                label_iter.textContent = label_iter.textContent.replace(master_label_language[0], '');
+              }
+            }
           }
           else {
             elm.classList.remove('field-language-' + code);
+            elm.classList.remove('active');
           }
         }
         else {
+          var active_code = pack_tabs.item_list.querySelector('.active').dataset.code;
+
+
           if (code == 'original') {
             elm.style.display = null;
-            elm.classList.add('active');
+            if (elm.classList.contains('field-language-' + active_code)) {
+              elm.classList.add('active');
+            }
+            if (master_label_language) {
+              var labels = elm.querySelectorAll('label');
+              for (let label_iter of labels) {
+                label_iter.textContent = label_iter.textContent + master_label_language[0];
+              }
+            }
           }
           else {
             elm.classList.add('field-language-' + code);
+            if (code == active_code) {
+              elm.classList.add('active');
+            }
           }
         }
       });
@@ -55,6 +90,23 @@ Drupal.behaviors.translation_moderation_tabs = {
       }
       for (other_select of other_selects) {
         other_select.selectedIndex = event.target.selectedIndex;
+      }
+    });
+
+    wrapper.addEventListener('change', (event) => {
+      if (event.target.tagName != 'SELECT') {
+        return;
+      }
+      var new_state = event.target.value;
+      var all_selects = wrapper.querySelectorAll('.field--name-moderation-state select');
+      var disabled = false;
+      for (let select_itr of all_selects) {
+        if (select_itr.value != new_state) {
+          disabled = true;
+        }
+      }
+      if (disabled) {
+        sync_states.disabled = true;
       }
     });
 
